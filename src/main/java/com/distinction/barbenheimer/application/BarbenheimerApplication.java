aaca3348903dde8c65b3d43051bd63d15516dd42
EddyEhhh <ehhitseddy@gmail.com>
2023-08-26 17:57:26 +0800

    Initial commit

---
+++ src/main/java/com/distinction/barbenheimer/application/BarbenheimerApplication.java
@@ -0,0 +1,13 @@
+package com.distinction.barbenheimer.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbenheimerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarbenheimerApplication.class, args);
+    }
+
+}
