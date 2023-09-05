e16ea56def4eb93bbcea6aabd5479338a8ed3edb
yjwrjh <jaquelineyee38@gmail.com>
2023-09-05 15:56:10 +0800

    added interface for movie service

--- src/main/java/com/distinction/barbenheimer/service/MovieService.java
+++ src/main/java/com/distinction/barbenheimer/service/MovieService.java
@@ -1,5 +1,15 @@
 package com.distinction.barbenheimer.service;
+import java.time.LocalDateTime;
+import java.util.*;
 
-public class MovieService {
+
+interface MovieService {
     
+    public List<Movie> getMovie(String movieName);
+
+    public Movie getMovieDetails(Movie movie);
+
+    public List<LocalDateTime> getMovieShowtimes(Movie movie);
+
+
 }
