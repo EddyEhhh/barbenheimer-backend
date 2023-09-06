3c9cee838e2652f12c9a8080fc31290b3f8a9291
yjwrjh <jaquelineyee38@gmail.com>
2023-09-06 13:45:20 +0800

    feat: get movie by title

--- src/main/java/com/distinction/barbenheimer/service/MovieServiceImpl.java
+++ src/main/java/com/distinction/barbenheimer/service/MovieServiceImpl.java
@@ -3,9 +3,23 @@ package com.distinction.barbenheimer.service;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.distinction.barbenheimer.model.Movie;
+import com.distinction.barbenheimer.repository.MovieRepository;
 
 public class MovieServiceImpl implements MovieService{
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public Movie getMovieByTitle(String movieTitle) {
+        Movie movie = movieRepository.findByTitle(movieTitle);
+        if (movie == null){
+            throw new RuntimeException("Movie does not exists");
+        }
+        return movie;
+}
 
     @Override
     public List<Movie> getMovie(String movieName) {
