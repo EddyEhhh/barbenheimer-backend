d198e239878fc90be9e8c204e677e310bb8c2605
Eddy <leoevinonyx@gmail.com>
2023-09-10 04:13:33 +0800

    depreciated: remove old models
    temporarily remove customer

--- src/main/java/com/distinction/barbenheimer/model/Customer.java
+++ src/main/java/com/distinction/barbenheimer/model/Customer.java
@@ -1,45 +1,45 @@
-package com.distinction.barbenheimer.model;
-
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
-import lombok.*;
-
-import java.time.LocalDate;
-import java.util.List;
-
-@Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class Customer {
-
-    @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
-    private Long id;
-
-    private int type;
-
-
-    @NotEmpty
-    private String username;
-
-    @NotEmpty
-    private String email;
-
-    @NotEmpty
-    private String defaultPayment;
-
-    @NotEmpty
-    private LocalDate birthDate;
-
-    private boolean isDeleted;
-
-    @OneToMany(mappedBy = "customer")
-    private List<Purchase> purchases;
-
-    @OneToMany(mappedBy = "customer")
-    private List<Review> reviews;
-
-
-}
+//package com.distinction.barbenheimer.model;
+//
+//import jakarta.persistence.*;
+//import jakarta.validation.constraints.NotEmpty;
+//import lombok.*;
+//
+//import java.time.LocalDate;
+//import java.util.List;
+//
+//@Entity
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Builder
+//public class Customer {
+//
+//    @Id
+//    @GeneratedValue(strategy= GenerationType.AUTO)
+//    private Long id;
+//
+//    private int type;
+//
+//
+//    @NotEmpty
+//    private String username;
+//
+//    @NotEmpty
+//    private String email;
+//
+//    @NotEmpty
+//    private String defaultPayment;
+//
+//    @NotEmpty
+//    private LocalDate birthDate;
+//
+//    private boolean isDeleted;
+//
+//    @OneToMany(mappedBy = "customer")
+//    private List<Purchase> purchases;
+//
+//    @OneToMany(mappedBy = "customer")
+//    private List<Review> reviews;
+//
+//
+//}
