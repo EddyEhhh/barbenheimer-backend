76e45a0e3cc1470201a45b22e0da953dd418e49b
yjwrjh <jaquelineyee38@gmail.com>
2023-09-13 18:57:43 +0800

    feat: added seatservice

---
+++ src/main/java/com/distinction/barbenheimer/service/SeatService.java
@@ -0,0 +1,18 @@
+package com.distinction.barbenheimer.service;
+
+import java.time.LocalDate;
+import java.util.*;
+
+import com.distinction.barbenheimer.model.CustomerDetail;
+import com.distinction.barbenheimer.model.Seat;
+
+public interface SeatService{
+    
+    //public int selected(long movieId, LocalDate movieSchedules, List<SeatSelectDTO> seats, CustomerDetail customerDetail);
+
+
+
+    public List<Seat> selectedBy(CustomerDetail customerDetail);
+
+
+}
