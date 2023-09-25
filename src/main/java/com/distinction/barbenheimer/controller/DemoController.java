b3c35cb3e329b5410da93d09ee87d070fbc16452
EddyEhhh <ehhitseddy@gmail.com>
2023-09-26 05:54:32 +0800

    chore: demo for ticket purchase event listener

--- src/main/java/com/distinction/barbenheimer/controller/DemoController.java
+++ src/main/java/com/distinction/barbenheimer/controller/DemoController.java
@@ -3,6 +3,7 @@ package com.distinction.barbenheimer.controller;
 import com.distinction.barbenheimer.DTO.MovieDetailsDTO;
 import com.distinction.barbenheimer.event.TicketPurchaseCompleteEvent;
 import com.distinction.barbenheimer.model.Purchase;
+import com.distinction.barbenheimer.repository.PurchaseRepository;
 import com.distinction.barbenheimer.service.MovieService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +21,12 @@ public class DemoController {
 
     private ApplicationEventPublisher applicationEventPublisher;
 
+    private PurchaseRepository purchaseRepository;
+
     @Autowired
-    public DemoController(ApplicationEventPublisher applicationEventPublisher){
+    public DemoController(ApplicationEventPublisher applicationEventPublisher, PurchaseRepository purchaseRepository){
         this.applicationEventPublisher = applicationEventPublisher;
+        this.purchaseRepository = purchaseRepository;
     }
 
     @GetMapping
@@ -33,7 +37,7 @@ public class DemoController {
 
     @GetMapping("/mail")
     public String demoMail(){
-        applicationEventPublisher.publishEvent(new TicketPurchaseCompleteEvent(new Purchase()));
+        applicationEventPublisher.publishEvent(new TicketPurchaseCompleteEvent(purchaseRepository.findById(Long.valueOf(1)).get()));
         return "emailing";
     }
 
