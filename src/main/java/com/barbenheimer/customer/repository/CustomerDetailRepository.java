bb1c0ef495851edad67676784da957d46c0f1714
yjwrjh <jaquelineyee38@gmail.com>
2023-10-05 16:01:25 +0800

    feat: added documentation and created tests for customerdetail

--- src/main/java/com/barbenheimer/customer/repository/CustomerDetailRepository.java
+++ src/main/java/com/barbenheimer/customer/repository/CustomerDetailRepository.java
@@ -3,7 +3,12 @@ package com.barbenheimer.customer.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.barbenheimer.customer.model.CustomerDetail;
+import java.util.List;
+
 
 public interface CustomerDetailRepository extends JpaRepository<CustomerDetail,Long>{
     
+    public CustomerDetail findById(long id);
+
+    public CustomerDetail findByEmail(String email);
 }
